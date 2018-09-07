86b855bdb15bc5c5f10a82c1ebc14b588d00fc48
shrpne <shrpne@gmail.com>
2018-09-07 13:17:57 +0300

    add seo pixels

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,8 @@ module.exports = {
         { src: '~/plugins/click-blur.js', ssr: false },
         { src: '~/plugins/seo-gtag.js', ssr: false },
         { src: '~/plugins/seo-ym.js', ssr: false },
+        { src: '~/plugins/seo-fb.js', ssr: false },
+        { src: '~/plugins/seo-vk.js', ssr: false },
     ],
     env: Object.assign({}, processEnv, dotEnv),
     /*
