3cca4e6dcad0cb9e39d00b25677295835ceab989
Daniil Dulin <daniildulin@gmail.com>
2018-09-11 19:33:05 +0300

    change variables

--- assets/variables.js
+++ assets/variables.js
@@ -5,5 +5,5 @@ export const TESTNET = 'testnet';
 export const MAINNET_COIN_NAME = 'BIP';
 export const TESTNET_COIN_NAME = 'MNT';
 export const EXPLORER_MAINNET_URL = process.env.APP_EXPLORER_MAINNET_URL || 'https://explorer.minter.network';
-export const EXPLORER_TESTNET_URL = process.env.APP_EXPLORER_TESTNET_URL || 'http://localhost:8000';
+export const EXPLORER_TESTNET_URL = process.env.APP_EXPLORER_TESTNET_URL || 'https://testnet.explorer.minter.network';
 export const EXPLORER_RTM_URL = process.env.APP_EXPLORER_RTM_URL || 'https://rtm.explorer.minter.network';
